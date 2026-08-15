b42141036696d6f74c6ac44422fd4574b9d8964b
soportephygital-maker <soportephygital@gmail.com>
2026-08-14 19:47:47 -0600

    Migrate Android app to AGP 9 built-in Kotlin

--- mobile-android/app/build.gradle.kts
+++ mobile-android/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -18,9 +17,5 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
     }
 }

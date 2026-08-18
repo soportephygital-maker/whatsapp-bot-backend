85c6786234c4449073edfe921123bc288ec037f3
soportephygital-maker <soportephygital@gmail.com>
2026-08-17 20:22:19 -0600

    Bump Android app to 0.4.2

--- mobile-android/app/build.gradle.kts
+++ mobile-android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.phygital.bot"
         minSdk = 26
         targetSdk = 37
-        versionCode = 6
-        versionName = "0.4.1"
+        versionCode = 7
+        versionName = "0.4.2"
     }
 
     compileOptions {

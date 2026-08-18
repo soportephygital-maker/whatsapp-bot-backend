842c9d2aa84da50b44ac4c3a31ca2dae080c20e0
soportephygital-maker <soportephygital@gmail.com>
2026-08-17 19:00:49 -0600

    Build Android app version 0.4.0

--- mobile-android/app/build.gradle.kts
+++ mobile-android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.phygital.bot"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "0.3.0"
+        versionCode = 5
+        versionName = "0.4.0"
     }
 
     compileOptions {

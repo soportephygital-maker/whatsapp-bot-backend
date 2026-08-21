a3b3445e2d3f3c009dccade503dfdf575c93d90b
soportephygital-maker <soportephygital@gmail.com>
2026-08-21 11:30:29 -0600

    Release Android 0.5.4

--- mobile-android/app/build.gradle.kts
+++ mobile-android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.phygital.bot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.5.3"
+        versionCode = 12
+        versionName = "0.5.4"
     }
 
     signingConfigs {

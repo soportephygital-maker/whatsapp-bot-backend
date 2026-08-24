bd978a6d14301163ec3c8f4e3281003d62f79296
soportephygital-maker <soportephygital@gmail.com>
2026-08-24 13:31:08 -0600

    Bump Android version for persistent background bridge

--- mobile-android/app/build.gradle.kts
+++ mobile-android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.phygital.bot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "0.6.3"
+        versionCode = 22
+        versionName = "0.6.4"
     }
 
     signingConfigs {

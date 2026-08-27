fdb2317e24c75b0f23db9191cf9e771e9bf9e2e6
soportephygital-maker <soportephygital@gmail.com>
2026-08-27 17:28:56 -0600

    chore: bump Android app to 0.6.5

--- mobile-android/app/build.gradle.kts
+++ mobile-android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.phygital.bot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 22
-        versionName = "0.6.4"
+        versionCode = 23
+        versionName = "0.6.5"
     }
 
     signingConfigs {

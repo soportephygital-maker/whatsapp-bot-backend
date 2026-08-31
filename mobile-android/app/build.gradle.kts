5997ca16e047a325c6d341297f9d1587fd9bea48
soportephygital-maker <soportephygital@gmail.com>
2026-08-31 14:41:42 -0600

    Bump Android app to 0.6.12 with native controls

--- mobile-android/app/build.gradle.kts
+++ mobile-android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.phygital.bot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 29
-        versionName = "0.6.11"
+        versionCode = 30
+        versionName = "0.6.12"
     }
 
     signingConfigs {

0adb905e3e9c597bb6abcf6c7eb5fcc7338fbcc9
soportephygital-maker <soportephygital@gmail.com>
2026-09-02 18:04:13 -0600

    Bump Android bridge to 0.6.18

--- mobile-android/app/build.gradle.kts
+++ mobile-android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.phygital.bot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 35
-        versionName = "0.6.17"
+        versionCode = 36
+        versionName = "0.6.18"
     }
 
     signingConfigs {

e26e47e65eb30ba989c8852551c947efa3502abf
soportephygital-maker <soportephygital@gmail.com>
2026-09-02 18:17:29 -0600

    Bump Android to 0.6.19 after keepalive unlock

--- mobile-android/app/build.gradle.kts
+++ mobile-android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.phygital.bot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 36
-        versionName = "0.6.18"
+        versionCode = 37
+        versionName = "0.6.19"
     }
 
     signingConfigs {

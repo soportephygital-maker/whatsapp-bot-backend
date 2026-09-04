7636279eab01fb87490d66946e44cc87bd6bfc58
soportephygital-maker <soportephygital@gmail.com>
2026-09-04 15:56:11 -0600

    Bump dashboard UI to 2026.09.04-60

--- app/routers/dashboard_fullscreen_support_patch.py
+++ app/routers/dashboard_fullscreen_support_patch.py
@@ -4,12 +4,13 @@ from fastapi.responses import HTMLResponse, Response
 from .login_recovery_dashboard_patch import _html as base_html, _js as base_js
 
 router = APIRouter(tags=['dashboard-ui-fullscreen-support'])
-UI_VERSION = '2026.09.04-59'
+UI_VERSION = '2026.09.04-60'
 
 
 def _html() -> str:
     html = base_html()
     for old in (
+        '2026.09.04-59',
         '2026.09.04-58',
         '2026.09.04-57',
         '2026.09.04-56',

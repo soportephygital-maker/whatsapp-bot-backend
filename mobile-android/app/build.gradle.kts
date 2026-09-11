f911414b0389f2807b99dce38ff3e958c851987b
soportephygital-maker <soportephygital@gmail.com>
2026-09-11 17:38:57 -0600

    Bump Android to 0.6.26 for support-only login

--- mobile-android/app/build.gradle.kts
+++ mobile-android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.phygital.bot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 43
-        versionName = "0.6.25"
+        versionCode = 44
+        versionName = "0.6.26"
     }
 
     signingConfigs {

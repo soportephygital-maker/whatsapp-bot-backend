b95a39b2cdf22d701996e19fa0c647ef83f753c2
soportephygital-maker <soportephygital@gmail.com>
2026-09-14 17:14:47 -0600

    Bump Android to 0.6.30 for session and updater fixes

--- mobile-android/app/build.gradle.kts
+++ mobile-android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.phygital.bot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 47
-        versionName = "0.6.29"
+        versionCode = 48
+        versionName = "0.6.30"
     }
 
     signingConfigs {

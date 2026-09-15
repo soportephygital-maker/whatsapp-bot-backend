35e4ad49a33d7d41aa88f4e3b590fbd63065223b
soportephygital-maker <soportephygital@gmail.com>
2026-09-15 14:03:53 -0600

    Enable global human and finish intents

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -3,3 +3,4 @@ from . import ticket_email_runtime_patch  # noqa: F401
 from . import image_flow_runtime_patch  # noqa: F401
 from . import natural_menu_runtime_patch  # noqa: F401
 from . import ticket_close_message_runtime_patch  # noqa: F401
+from . import global_intent_runtime_patch  # noqa: F401

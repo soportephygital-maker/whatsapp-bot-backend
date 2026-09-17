3b80285fc32e4d6bfcea0a7b15bda943ed9b398c
soportephygital-maker <soportephygital@gmail.com>
2026-09-17 16:32:31 -0600

    Publish Android version 7.0

--- mobile-android/app/build.gradle.kts
+++ mobile-android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.phygital.bot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 68
-        versionName = "0.6.32"
+        versionCode = 69
+        versionName = "7.0"
     }
 
     signingConfigs {

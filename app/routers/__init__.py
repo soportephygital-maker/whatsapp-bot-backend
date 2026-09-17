c19ef108298fc891ca3e0894b9736a5a8ec1e4c8
soportephygital-maker <soportephygital@gmail.com>
2026-09-17 13:54:51 -0600

    Activate finish guard runtime patch

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,3 +1,4 @@
 from . import activate_post_close_patch  # noqa: F401
 from . import ticket_email_runtime_patch  # noqa: F401
+from . import finish_guard_runtime_patch  # noqa: F401
 from . import dashboard_version_runtime_patch  # noqa: F401

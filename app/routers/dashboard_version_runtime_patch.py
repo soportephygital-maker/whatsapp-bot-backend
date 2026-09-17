88fb9b3101f57a5c794b1093d6752506f16c5ddc
soportephygital-maker <soportephygital@gmail.com>
2026-09-17 11:14:34 -0600

    Bump dashboard UI to 2026.09.04-107

---
+++ app/routers/dashboard_version_runtime_patch.py
@@ -0,0 +1,16 @@
+from . import dashboard_email_milestone_patch as target
+
+UI_VERSION = '2026.09.04-107'
+_original_html = target._html
+
+
+def _html():
+    html = _original_html()
+    for old in ('2026.09.04-105', '2026.09.04-106'):
+        html = html.replace(f'UI {old}', f'UI {UI_VERSION}')
+        html = html.replace(f'/dashboard.js?v={old}', f'/dashboard.js?v={UI_VERSION}')
+    return html
+
+
+target.UI_VERSION = UI_VERSION
+target._html = _html

695fe3c322e8d0830b4cb224e760e6088c8711f2
soportephygital-maker <soportephygital@gmail.com>
2026-09-17 13:56:18 -0600

    Activate Android image upload route fix

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,4 +1,5 @@
 from . import activate_post_close_patch  # noqa: F401
 from . import ticket_email_runtime_patch  # noqa: F401
 from . import finish_guard_runtime_patch  # noqa: F401
+from . import case_media_route_runtime_patch  # noqa: F401
 from . import dashboard_version_runtime_patch  # noqa: F401

aecf2adb9cc18c452c0164babcc99a042da95e6d
soportephygital-maker <soportephygital@gmail.com>
2026-09-17 12:24:06 -0600

    Bump Android build to 64 for listener diagnostics

--- mobile-android/app/build.gradle.kts
+++ mobile-android/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.phygital.bot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 63
+        versionCode = 64
         versionName = "0.6.32"
     }
 

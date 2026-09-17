0495a511cddf931c0f015bbe3536de996bbe9671
soportephygital-maker <soportephygital@gmail.com>
2026-09-17 10:20:47 -0600

    Bump Android to 0.6.35 build 53

--- mobile-android/app/build.gradle.kts
+++ mobile-android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.phygital.bot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 52
-        versionName = "0.6.34"
+        versionCode = 53
+        versionName = "0.6.35"
     }
 
     signingConfigs {
